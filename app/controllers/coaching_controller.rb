32c94570957c123491ec519fa9e64568cef5df07
Alex Ratner <github@unsealed.net>
2017-03-23 13:12:00 +0100

    implement answer

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,5 +1,21 @@
 class CoachingController < ApplicationController
   def answer
+    @query = params[:query]
+    if @query.upcase == "I am going to work right now!".upcase
+      @response = ''
+    elsif @query.end_with?'?'
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+
+    if @response == ''
+      ''
+    elsif @query == @query.upcase
+      @response = "I can feel your motivation! " + @response
+    else
+      @response
+    end
   end
 
   def ask
